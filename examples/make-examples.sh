fb8722d258cd11a0c5ea63fa96c66f4e6a1b8ef3
Kyle Kestell <kyle@kestell.org>
2024-05-31 14:01:18 -0500

    .

--- examples/make-examples.sh
+++ examples/make-examples.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
+set -x
 
-URL='https://www.epicurious.com/recipes/food/views/philly-fluff-cake'
+urls=(
+  'https://www.bonappetit.com/recipe/bas-best-chocolate-chip-cookies'
+  'https://smittenkitchen.com/2022/05/double-chocolate-chip-muffins/'
+  'https://www.foodandwine.com/recipes/jamaican-jerk-chicken'
+  'https://www.allrecipes.com/recipe/7399/tres-leches-milk-cake/'
+  'https://www.epicurious.com/recipes/food/views/flourless-chocolate-cake-14478'
+)
 
 pushd "$(dirname "$0")" || exit 1
 
-rf -o example.pdf $URL || exit 1
-convert -density 100 -quality 100 -flatten example.pdf example.jpg
+for i in "${!urls[@]}"; do
+    url="${urls[i]}"
 
-rf -o example-cleaned.pdf -c $URL || exit 1
-convert -density 100 -quality 100 -flatten example-cleaned.pdf example-cleaned.jpg
+    python ../main.py -m gpt-4o -o "example$(($i+1)).pdf" "$url" || exit 1
+    convert -density 100 -quality 100 -flatten "example$(($i+1)).pdf" "example$(($i+1)).jpg"
+
+    python ../main.py -m gpt-4o -o "example$(($i+1))-cleaned.pdf" -c "$url" || exit 1
+    convert -density 100 -quality 100 -flatten "example$(($i+1))-cleaned.pdf" "example$(($i+1))-cleaned.jpg"
+done
 
 popd || exit 1
