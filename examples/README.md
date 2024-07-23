f28862886f99ab63f3debe60cc1710fddf4ef4be
Kyle Kestell <kyle@kestell.org>
2024-07-23 08:16:13 -0500

    Update examples

--- examples/README.md
+++ examples/README.md
@@ -7,25 +7,17 @@ URL='https://www.allrecipes.com/recipe/17644/german-chocolate-cake-iii/'
 ```
 
 | ![Example 1](example1-1.jpg) |
-|:-------------------------------------:|
-|       `rf -o example1.pdf $URL`       |
+|:----------------------------:|
+|  `rf -o example1.pdf $URL`   |
 
 | ![Example 2](example2-1.jpg) |
-|:-------------------------------------:|
-|     `rf -o example2.pdf -n $URL`      |
+|:----------------------------:|
+| `rf -o example2.pdf -n $URL` |
 
-| ![Example 3](example3-1.jpg) |
-|:-------------------------------------:|
-|    `rf -o example3.pdf -n -g $URL`    |
+|  ![Example 3](example3-1.jpg)   |
+|:-------------------------------:|
+| `rf -o example3.pdf -n -g $URL` |
 
-| ![Example 4](example4-1.jpg) ![Example 4](example4-2.jpg) |
-|:---------------------------------------------------------------------------:|
-|                    `rf -t -o example4.pdf -n -g -t $URL`                    |
-
-| ![Example 5](example5-1.jpg) ![Example 5](example5-2.jpg) |
-|:---------------------------------------------------------------------------:|
-|           `rf -o example5.pdf -n -g -t -r "sub goat's milk" $URL`           |
-
-| ![Example 6](example6-1.jpg) ![Example 6](example6-2.jpg) |
-|:---------------------------------------------------------------------------:|
-|        `rf -o example6.pdf -n -g -t -r "sub goat's milk" -s 2 $URL`         |
+|             ![Example 4](example4-1.jpg)             |
+|:----------------------------------------------------:|
+| `rf -o example4.pdf -n -g -r "sub goat's milk" $URL` |
--- examples/make-examples.sh
+++ examples/make-examples.sh
@@ -5,22 +5,17 @@ pushd "$(dirname "$0")" || exit 1
 
 URL="https://www.allrecipes.com/recipe/17644/german-chocolate-cake-iii/"
 
-python ../main.py -o example1.pdf -v $URL || exit 1
+pdm run python ../src/recipe_formatter/main.py -o example1.pdf -v $URL || exit 1
 gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dJPEGQ=100 -r150 -sOutputFile=example1-%d.jpg example1.pdf
 
-python ../main.py -o example2.pdf -v -n $URL || exit 1
+pdm run python ../src/recipe_formatter/main.py -o example2.pdf -v -n $URL || exit 1
 gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dJPEGQ=100 -r150 -sOutputFile=example2-%d.jpg example2.pdf
 
-python ../main.py -o example3.pdf -v -n -g $URL || exit 1
+pdm run python ../src/recipe_formatter/main.py -o example3.pdf -v -n -g $URL || exit 1
 gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dJPEGQ=100 -r150 -sOutputFile=example3-%d.jpg example3.pdf
 
-python ../main.py -o example4.pdf -v -n -g -t $URL || exit 1
+pdm run python ../src/recipe_formatter/main.py -o example4.pdf -v -n -g -r "sub goat's milk" $URL || exit 1
 gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dJPEGQ=100 -r150 -sOutputFile=example4-%d.jpg example4.pdf
 
-python ../main.py -o example5.pdf -v -n -g -t -r "sub goat's milk" $URL || exit 1
-gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dJPEGQ=100 -r150 -sOutputFile=example5-%d.jpg example5.pdf
-
-python ../main.py -o example6.pdf -v -n -g -t -r "sub goat's milk" -s 2 $URL || exit 1
-gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dJPEGQ=100 -r150 -sOutputFile=example6-%d.jpg example6.pdf
 
 popd || exit 1
